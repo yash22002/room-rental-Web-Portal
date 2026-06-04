cd8df5c5b44b60816c0717935c3de702649833f8
Yash Saxena <161794563+yash22002@users.noreply.github.com>
2026-06-04 22:52:45 +0530

    Update config.php

--- config.php
+++ config.php
@@ -9,5 +9,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-session_start(); // Session start karna zaroori hai role-tracking ke liye
+session_start();
 ?>
