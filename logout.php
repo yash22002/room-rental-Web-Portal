c2aeabdda73b18b2c777d6a3e7ded429d8b2ef54
Yash Saxena <161794563+yash22002@users.noreply.github.com>
2026-06-04 22:54:45 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
-// Session start karna zaroori hai taaki use destroy kiya ja sake
 session_start();
 
-// Saare session variables ko remove karein
 $_SESSION = array();
 
-// Session ko puri tarah khatam karein
 session_destroy();
 
-// User ko login page (Account.html) par bhej dein
 header("Location: Account.html");
 exit();
 ?>
