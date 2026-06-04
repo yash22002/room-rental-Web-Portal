9b1aa9edfea5d71923c60598fb8b139d0fb5215a
Yash Saxena <161794563+yash22002@users.noreply.github.com>
2026-06-04 22:54:24 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,21 +6,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = $_POST['password'];
 
-    // User ko find karein
     $sql = "SELECT * FROM users WHERE email = '$email' AND user_role = '$role'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) === 1) {
         $user = mysqli_fetch_assoc($result);
         
-        // Encrypted password verify karein
         if (password_verify($password, $user['password'])) {
-            // Session variables set karein
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_role'] = $user['user_role'];
             $_SESSION['user_name'] = $user['full_name'];
 
-            // Role based redirection
             if ($role === "Owner") {
                 header("Location: Owner.php");
             } else {
